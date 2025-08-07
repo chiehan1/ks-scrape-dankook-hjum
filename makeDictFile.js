e6d123b787b059529d89b2061f21eb1bb3e202d7
chiehan1 <chiehan1@users.noreply.github.com>
2025-08-07 12:38:15 +0800

    update hj

--- makeDictFile.js
+++ makeDictFile.js
@@ -5,6 +5,7 @@ import naturalSort from 'javascript-natural-sort'
 import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'fs'
 import { basename, resolve } from 'path'
 import { addHjumLink } from './src/addHjumLink.js'
+import { addHjLink } from './src/addHjLink.js'
 
 const importCss = '<link rel="stylesheet" href="style/base.css"><link rel="stylesheet" href="style/content.css"><link rel="stylesheet" href="style/common.css">'
 const emptyJsRegex = /<script [^>]+?>[\s\S]+?<\/script>/g
@@ -106,49 +107,6 @@ function addHhVocaLink(txtResult) {
 function addHhWordLink(txtResult) {
   const linkLexemeSets = []
   const table = readFileSync('./lexemeTable/hhWordList.html', 'utf8')
-
-  const lexemeStrsRegex = />([^<]*?)<\/a/g
-  const trsRegex = /<td>\d+<([\s\S]+?)<\/tr>/g
-  const trs = getRegexStrs(table, trsRegex)
-
-  for (const tr of trs) {
-    //console.log(tr)
-    const lexemeStrs = getRegexStrs(tr, lexemeStrsRegex)
-
-    const mainLexemes = getLexemes(lexemeStrs.shift()) 
-
-    for (const mainLexeme of mainLexemes) {
-      let linkLexemes = []
-
-      findQuoteAndStarLexeme(mainLexeme, linkLexemes)
-
-      for (const lexemeStr of lexemeStrs) {
-        const lexemes = getLexemes(lexemeStr)
-        
-        for (const lexeme of lexemes) {
-          linkLexemes.push(lexeme)
-          findQuoteAndStarLexeme(lexeme, linkLexemes)
-        }
-      }
-
-      linkLexemes = [ ...new Set(linkLexemes) ]
-
-      for (const linkLexeme of linkLexemes) {
-        linkLexemeSets.push([ linkLexeme, mainLexeme ])
-      } 
-    }
-  }
-
-  for (const [ linkLexeme, mainLexeme ] of linkLexemeSets) {
-    txtResult += `${linkLexeme}\r\n@@@LINK=${mainLexeme}\r\n</>\r\n`
-  }
-
-  return txtResult
-}
-
-function addHjLink(txtResult) {
-  const linkLexemeSets = []
-  const table = readFileSync('./lexemeTable/hjWordList.html', 'utf8')
 
   const lexemeStrsRegex = />([^<]*?)<\/a/g
   const trsRegex = /<td>\d+<([\s\S]+?)<\/tr>/g
